5486ae60becca1919df3381aa2d4cfabe1f989ae
Landtalk <landtalk2025@gmail.com>
2026-08-02 22:40:39 +0900

    최신 사이트 반영: lib/constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,7 @@
-export const BRAND_NAME = "무인아이스크림 인수연구소";
+import { siteConfig } from "../src/config/siteConfig";
 
-// 실제 판매 페이지가 확정되면 아래 주소만 교체하세요.
-export const SMART_STORE_EBOOK_URL = "https://smartstore.naver.com/heidelberg2018/products/12029387510";
-export const SITE_URL = "https://iceinsoo.kr";
+export const BRAND_NAME = siteConfig.brandName;
+export const SMART_STORE_EBOOK_URL = siteConfig.smartStoreEbookUrl;
+export const SITE_URL = siteConfig.siteUrl;
+export const CHECKLIST_URL = siteConfig.checklistUrl;
 
