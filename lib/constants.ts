02003626184e680bb0e18860a96ae5c900f9e5bf
Landtalk <landtalk2025@gmail.com>
2026-08-02 23:12:54 +0900

    fix: make deployment URL constants static

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
-import { siteConfig } from "../src/config/siteConfig";
-
-export const BRAND_NAME = siteConfig.brandName;
-export const SMART_STORE_EBOOK_URL = siteConfig.smartStoreEbookUrl;
-export const SITE_URL = siteConfig.siteUrl;
-export const CHECKLIST_URL = siteConfig.checklistUrl;
-
+export const SITE_URL = "https://iceinsoo.kr";
+export const BRAND_NAME = "무인아이스크림 인수연구소";
+export const SMART_STORE_EBOOK_URL = "https://smartstore.naver.com/heidelberg2018/products/12029387510";
+export const CHECKLIST_URL = `${SITE_URL}/checklist-download`;
+export const SEO_TITLE = "무인아이스크림 인수 계산기｜월순이익·권리금 회수기간 무료 진단";
+export const SEO_DESCRIPTION = "운영 중인 무인아이스크림 할인점의 월매출, 원가율, 월세, 관리비와 권리금을 입력해 예상 월순이익과 권리금 회수기간을 무료로 계산하세요. 실제 운영 경험을 바탕으로 인수 전 확인사항도 제공합니다.";
+export const OG_IMAGE = "/og.png";
