29bcefe586bf3c54fd41d4a32bd7f8db801083ab
Landtalk <landtalk2025@gmail.com>
2026-08-06 22:47:12 +0900

    Add operating guide: app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./acquisition.css";
 import "./scroll.css";
 import "./about.css";
 import "./footer.css";
+import "./guide.css";
 import { BRAND_NAME, OG_DESCRIPTION, OG_IMAGE, SEO_DESCRIPTION, SEO_TITLE, SITE_URL } from "../lib/constants";
 import { KakaoFloatingButton } from "../src/components/KakaoChannelButton";
 
